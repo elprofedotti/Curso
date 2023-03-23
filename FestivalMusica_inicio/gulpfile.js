16c150a4b24e5386165764dbaa96f745295cd45b
elprofedotti <elprofedotti@gmail.com>
2023-03-23 16:16:46 -0300

    seguimos

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -26,12 +26,16 @@ function css(done){
 const cache= require('gulp-cache');
 const imagemin=require('gulp-imagemin');
 const webp=require('gulp-webp');
+const avif=require('gulp-avif');
+
+
 function imagenes( done){
     const opciones={
-        quality: 50
+        optimizatinLevel: 3
     };
     src('src/img/**/*.{png,jpg}')
-        .pipe( cache(imagemin()))
+        .pipe( cache(imagemin(opciones)))
+        .pipe(dest('build/img'));
     done();
 }
 function versionWebp( done){
@@ -44,11 +48,23 @@ function versionWebp( done){
     done();
 }
 
+function versionAvif( done){
+    const opciones={
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones))
+        .pipe(dest('build/img'));
+    done();
+}
+
 function dev(done){
     watch('src/scss/**/*.scss', css);
     done();
 }
 
 exports.css=css;
+exports.imagenes=imagenes;
 exports.versionWebp=versionWebp;
-exports.dev=parallel( versionWebp, dev);
+exports.versionAvif=versionAvif;
+exports.dev=parallel( imagenes, versionWebp, versionAvif, dev);
